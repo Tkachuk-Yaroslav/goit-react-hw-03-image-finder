fe37fdd8ac82d148d33e4d53dd40c80d6f6b758d
Tkachuk-Yaroslav <tkachuky105@gmail.com>
2023-09-14 17:09:32 +0300

    Добавив апі
    Все що в Арр в коментах - нічого не вийшло

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,61 @@
+// import getImg from 'services/gallaryApi';
 import { Loader } from './Loader/Loader';
 import Searchbar from './Searchbar/Searchbar';
-
-// export const App = () => {
-//   return (
-//     <div>
-//       React homework template
-//       <Searchbar />
-//     </div>
-//   );
-// };
 import React, { Component } from 'react';
+// import { toast } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
 
 export default class App extends Component {
-  state = { searchQuery: '', isLoading: false };
+  state = {
+    searchQuery: '',
+    isLoading: false,
+    page: 1,
+    images: [],
+    error: null,
+  };
+
+  // componentDidUpdate(prevProps, prevState) {
+  //   console.log('componentDidUpdate');
+  //   const { searchQuery, page } = this.state;
+  //   if (prevState.searchQuery !== searchQuery || prevState.page !== page) {
+  //     this.getImages(searchQuery, page);
+  //   }
+  // }
+
+  // getImages = async (query, page) => {
+  //   if (!query) {
+  //     return console.log('return');
+  //   }
+  //   this.setState({
+  //     searchQuery: query,
+  //     page,
+  //     isLoading: true,
+  //   });
+  //   try {
+  //     const { hits, totalHits } = await getImg(query, page);
+  //     console.log('hits>>', hits);
+  //     if (hits.length === 0) {
+  //       // this.setState({ loadMore: false });
+  //       alert('Nothing was found for your request. Try something else');
+  //     } else {
+  //       this.setState(
+  //         prevState => ({
+  //           images: [...prevState.images, ...hits],
+  //           //loadMore:
+  //           //this.state.page < Math.ceil(totalHits / this.state.per_page),
+  //           //status: Status.RESOLVED,
+  //         }),
+  //         () => console.log('КОЛБЕК', this.state)
+  //       );
+  //       console.log(this.state);
+  //     }
+  //   } catch (error) {
+  //     // this.setState({ error: error.message, status: Status.REJECTED });
+  //     console.log(error);
+  //   } finally {
+  //     this.setState({ isLoading: false });
+  //   }
+  // };
 
   onFormSubmit = dataFromSearchbar => {
     this.setState({ searchQuery: dataFromSearchbar });
--- src/services/gallaryApi.js
+++ src/services/gallaryApi.js
@@ -3,9 +3,11 @@ axios.defaults.baseURL = 'https://pixabay.com/api/';
 
 const MY_KEY = '38626503-0c946b31d6d08b2c506c34012';
 
-export const getImg = async () => {
+const getImg = async (query, page) => {
   const { data } = await axios(
-    `?q=cat&page=1&key=${MY_KEY}&image_type=photo&orientation=horizontal&per_page=12`
+    `?q=${query}&page=${page}&key=${MY_KEY}&image_type=photo&orientation=horizontal&per_page=12`
   );
   return data;
 };
+
+export default getImg;
